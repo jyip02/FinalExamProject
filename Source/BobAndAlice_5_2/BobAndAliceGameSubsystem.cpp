3ef329a706c853207e566257cd2de082e83f657f
jyip02 <jasonyip28.2001@gmail.com>
2023-12-13 23:37:25 -0500

    1st commit

---
+++ Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.cpp
@@ -0,0 +1,10 @@
+﻿#include "BobAndAliceGameSubsystem.h"
+
+void UBobAndAliceGameSubsystem::Initialize(
+    FSubsystemCollectionBase &Collection) {
+  Super::Initialize(Collection);
+}
+
+void UBobAndAliceGameSubsystem::Deinitialize() { Super::Deinitialize(); }
+
+
